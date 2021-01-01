7db33e89a57ffd053e6b20b7e59b5f424945c825
unknown <tanche98@gmail.com>
2021-01-01 16:36:30 +0200

    init security implementation

---
+++ src/main/java/bg/sofia/uni/fmi/piss/project/wevip/repository/WevipUserRepository.java
@@ -0,0 +1,9 @@
+package bg.sofia.uni.fmi.piss.project.wevip.repository;
+
+import bg.sofia.uni.fmi.piss.project.wevip.model.WevipUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WevipUserRepository extends JpaRepository<WevipUser, Integer> {
+}
