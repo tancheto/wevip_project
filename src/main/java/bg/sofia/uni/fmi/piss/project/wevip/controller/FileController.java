31cbcd6e1cc5dcd47ffccd368e9dbd2bb7bd5ae8
unknown <tanche98@gmail.com>
2021-01-17 20:46:43 +0200

    final fix upload file
    # Conflicts:
    #	src/main/resources/templates/settings.html

--- src/main/java/bg/sofia/uni/fmi/piss/project/wevip/controller/FileController.java
+++ src/main/java/bg/sofia/uni/fmi/piss/project/wevip/controller/FileController.java
@@ -4,6 +4,7 @@ import bg.sofia.uni.fmi.piss.project.wevip.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +18,10 @@ public class FileController {
     @Autowired
     FileService fileService;
 
-    @PostMapping("/upload")
+    @PostMapping("/upload/{username}")
     public ResponseEntity uploadFile(@RequestParam("profile_pic") MultipartFile file,
-                                     @RequestParam("username_form") String username) {
+                                     @PathVariable String username) {
+                                     //@RequestParam("username_form") String username) {
 
         return fileService.uploadFile(file, username);
     }
--- src/main/resources/public/js/ui/settings-script.js
+++ src/main/resources/public/js/ui/settings-script.js
@@ -1,26 +1,25 @@
-
-document.getElementById("a-events-top30").addEventListener("click", function (e) {
+document.getElementById("a-events-top30").addEventListener("click", function(e) {
     console.log("clickeddeded 30");
     localStorage.setItem("active-events", "top30");
     console.log("active tab = ", localStorage.getItem("active-events"));
 });
-document.getElementById("a-events-all").addEventListener("click", function (e) {
+document.getElementById("a-events-all").addEventListener("click", function(e) {
     localStorage.setItem("active-events", "all");
     console.log("active tab = ", localStorage.getItem("active-events"));
 });
 
 
-document.addEventListener("DOMContentLoaded", function(event){
-	document.getElementById("username").innerText = localStorage.getItem("username");
-	getCurrentUser();
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("username").innerText = localStorage.getItem("username");
+    getCurrentUser();
 })
 
-function ShowSaveConfirmation(){
-	document.getElementById("save-info-message").style.visibility="visible";
+function ShowSaveConfirmation() {
+    document.getElementById("save-info-message").style.visibility = "visible";
 }
 
-function HideSaveConfirmation(){
-	document.getElementById("save-info-message").style.visibility="hidden";
+function HideSaveConfirmation() {
+    document.getElementById("save-info-message").style.visibility = "hidden";
 }
 
 function getCurrentUser() {
@@ -28,13 +27,13 @@ function getCurrentUser() {
         type: "POST",
         contentType: "application/json",
         url: "/user/current/" + sessionStorage.getItem('username'),
-        success: function (user) {
-			console.log(user);
-			document.getElementById("username-h2").innerHTML = "<h2>Username: "+user.username+"</h2>";
-			document.getElementById("email-h2").innerHTML = "<h2>Email: "+user.email+"</h2>";
+        success: function(user) {
+            console.log(user);
+            document.getElementById("username-h2").innerHTML = "<h2>Username: " + user.username + "</h2>";
+            document.getElementById("email-h2").innerHTML = "<h2>Email: " + user.email + "</h2>";
 
         },
-        error: function (e) {
+        error: function(e) {
             console.log("ERROR: ", e);
         }
     });
@@ -43,30 +42,54 @@ function getCurrentUser() {
 function onFileUploadSubmit() {
     document.getElementById("username_form").value = sessionStorage.getItem('username');
 
-    $('#profile_pic_upload')
-    .ajaxForm({
-            url: '/file/upload',
-            type: "POST",
-            success: function(response) {
-                alert("The server says: " + response);
-            }
-        });
+    $('#profile_pic_upload').ajaxForm({
+        url: "/file/upload",
+        type: "POST",
+        success: function(response) {
+            alert("The server says: " + response);
+        },
+        error: function(e) {
+            console.log("ERROR: ", e);
+        }
+    });
+}
+
+function formSubmit(e) {
+    e.preventDefault(); //This will prevent the default click action
+
+    var formData = new FormData();
+    var input = document.getElementById("profile_pic");
+    var file = input.files[0];
+    formData.append("profile_pic", file);
+
+    $.ajax({
+        type: 'POST',
+        url: "/file/upload/" + sessionStorage.getItem('username'),
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+            alert("The server says: " + response);
+        },
+        error: function(e) {
+            alert("ERROR: ", e);
+        }
+    });
 }
 
 function readURL(input) {
-	if (input.files && input.files[0]) {
-	  var reader = new FileReader();
-	  
-	  reader.onload = function(e) {
-		$('#pic_preview').attr('src', e.target.result);
-	  }
-	  
-	  reader.readAsDataURL(input.files[0]); // convert to base64 string
-	}
-  }
-  
-  $("#profile_pic").change(function() {
-	document.getElementById('uploaded-h2').style.visibility = "visible";
-	readURL(this);
-  });
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
+        reader.onload = function(e) {
+            $('#pic_preview').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+}
+
+$("#profile_pic").change(function() {
+    document.getElementById('uploaded-h2').style.visibility = "visible";
+    readURL(this);
+});
